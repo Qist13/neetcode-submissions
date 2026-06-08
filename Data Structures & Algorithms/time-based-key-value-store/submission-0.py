426842ec3f6149b88d64a8fc9840ee1ab67b7724
qkxia <75295056+Qist13@users.noreply.github.com>
2026-06-08 16:56:59 -0700

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.py
@@ -0,0 +1,24 @@
+class TimeMap:
+
+    def __init__(self):
+        self.hash_map = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.hash_map[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        arr = self.hash_map[key]
+        left, right = 0, len(arr) - 1
+
+        result = ""
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if arr[mid][1] <= timestamp:
+                result = arr[mid][0]
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return result
